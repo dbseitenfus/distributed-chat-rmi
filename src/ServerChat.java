24f0e5257f3edf76eec22b0dcdaf75e32947f4b0
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-05 15:51:21 -0300

    feat: create server gui

--- src/ServerChat.java
+++ src/ServerChat.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.LocateRegistry;
@@ -10,8 +13,68 @@ import java.util.List;
 public class ServerChat extends UnicastRemoteObject implements IServerChat {
     private Map<String, IRoomChat> roomList;
 
+    //GUI
+    private DefaultTableModel tableModel;
+
+
     public ServerChat() throws RemoteException {
         roomList = new HashMap<>();
+        setupGUI();
+    }
+
+    private void setupGUI() {
+        JFrame frame = new JFrame("ServerChat");
+        frame.setSize(400, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] columnNames = {"Sala"};
+        tableModel = new DefaultTableModel(columnNames, 0);
+        JTable table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        JButton closeButton = new JButton("Fechar Sala Selecionada");
+        closeButton.addActionListener(e -> {
+            int selectedRow = table.getSelectedRow();
+            if (selectedRow != -1) {
+                String roomName = (String) tableModel.getValueAt(selectedRow, 0);
+                System.out.println("Sala selecionada: " + selectedRow + ", nome: " + roomName);
+                closeRoom(roomName);
+            } else {
+                JOptionPane.showMessageDialog(frame, "Selecione uma sala para fechar.");
+            }
+        });
+
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        bottomPanel.add(closeButton);
+
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+        updateRoomTable();
+    }
+
+    private void closeRoom(String roomName) {
+        try {
+            IRoomChat room = roomList.get(roomName);
+            if (room != null) {
+                room.closeRoom();
+                roomList.remove(roomName);
+                Registry registry = LocateRegistry.getRegistry(2020);
+                registry.unbind(roomName);
+                System.out.println("Sala fechada via RMI: " + roomName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        updateRoomTable();
+    }
+
+    private void updateRoomTable() {
+        tableModel.setRowCount(0);
+        for (String roomName : roomList.keySet()) {
+            tableModel.addRow(new Object[]{roomName});
+        }
     }
 
     @Override
@@ -28,6 +91,7 @@ public class ServerChat extends UnicastRemoteObject implements IServerChat {
                 registry.rebind(roomName, room);
                 roomList.put(roomName, room);
                 System.out.println("Sala criada e registrada: " + roomName);
+                updateRoomTable();
             } catch (Exception e) {
                 e.printStackTrace();
             }
